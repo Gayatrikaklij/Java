8353866af3b80985a74890ac28c60cde06bbb251
Gayatrikaklij <157037947+Gayatrikaklij@users.noreply.github.com>
2025-07-25 20:01:53 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class one {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String name=sc.nextLine();
+        String AY=sc.nextLine();
+
+        System.out.println("hello " + name+"," + "You are in " + AY );
+    }
+    
+}
